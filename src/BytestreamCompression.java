3ca09877753937e6059cd4e9fb74ba865f2352db
Melli <melaniemueller.21@web.de>
2020-09-06 22:30:15 +0200

    10 times

--- src/BytestreamCompression.java
+++ src/BytestreamCompression.java
@@ -3,7 +3,10 @@ public class BytestreamCompression {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello There");
+		for(int i=0; i<10; i++) {
+			System.out.println("Hello There");
+		}
+		
 	}
 
 }
